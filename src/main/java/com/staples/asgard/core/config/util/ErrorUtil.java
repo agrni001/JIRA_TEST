a24162d6f42259e160359dd10637e060057e1f41
Gandhi, Rajmeet Singh <RajmeetSingh.Gandhi@Staples.com>
2014-04-09 12:26:51 +0530

    Minor changes in error codes and logging approach

--- src/main/java/com/staples/asgard/core/config/util/ErrorUtil.java
+++ src/main/java/com/staples/asgard/core/config/util/ErrorUtil.java
@@ -11,16 +11,20 @@ public class ErrorUtil {
 	
 	private static final Logger LOG = LoggerFactory.getLogger(ErrorUtil.class);
 
-
 	/**
-	 * Creates and Converts the AsgardError object to JSON string.
+	 * Creates and Converts the AsgardError object to JSON string. 
+	 * It is also a common point where errors are logged
 	 * 
 	 * @param restError
 	 * @return output JSON in String format
 	 */
-	public static String createErrorJson(AsgardError asgardError) {
+	public static String createErrorJson(AsgardError asgardError, String errorMessage) {
+		
+		//Log the error 
+		LOG.error("Error Code = {} : Error Message = {}", asgardError.getCode(), errorMessage);
 		
 		String outputErrorJSON = null;		
+				
 		/** Creating the RestError object **/
 		RestError restError = new RestError();
 		restError.addErrors(asgardError.getCode(), asgardError.getCustomMessage());
@@ -34,4 +38,6 @@ public class ErrorUtil {
 		}
 		return outputErrorJSON;
 	}
+	
+
 }
