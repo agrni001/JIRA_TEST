7bd4fe4bbf2d6d2255c6805431738fdc8af555f9
Gandhi, Rajmeet Singh <RajmeetSingh.Gandhi@Staples.com>
2014-04-10 15:04:09 +0530

    Error Code and error json being returned now

--- src/main/java/com/staples/asgard/core/constants/ErrorConstants.java
+++ src/main/java/com/staples/asgard/core/constants/ErrorConstants.java
@@ -57,4 +57,8 @@ public class ErrorConstants {
 	public static final AsgardError ERROR_BSSI002 = new AsgardError("BSSI002", "Store Inventory Integration is InActive");
 	public static final AsgardError ERROR_BSSI003 = new AsgardError("BSSI003", "Error in Store Inventory response");	
 	
+	//Online Inventory  Errors 
+	public static final AsgardError ERROR_BSOI001 = new AsgardError("BSOI001", "Error in Online Inventory response");
+	public static final AsgardError ERROR_BSOI002 = new AsgardError("BSOI002", "Online Inventory Integration is InActive");
+	public static final AsgardError ERROR_BSOI003 = new AsgardError("BSOI003", "Error in Online Inventory response");	
 }
