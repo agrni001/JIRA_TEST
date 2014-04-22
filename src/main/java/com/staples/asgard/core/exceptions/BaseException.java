c6e2f851e35218cd728a6045ef9d6c4e57da6d81
srani <srani@BLRSRANI33468.sapient.com>
2014-04-22 16:34:14 +0530

    Signed-off-by: srani <srani@BLRSRANI33468.sapient.com>

--- src/main/java/com/staples/asgard/core/exceptions/BaseException.java
+++ src/main/java/com/staples/asgard/core/exceptions/BaseException.java
@@ -1,21 +1,19 @@
 package com.staples.asgard.core.exceptions;
 
-
 /**
- * BaseException is the base class for all exceptions thrown within Asgard applications.
+ * BaseException is the base class for all exceptions thrown within Asgard
+ * applications.
  * 
  * @author
  */
 public class BaseException extends Exception {
-    
-    /**
+
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private  AsgardError asgardError ;
-    
+	private AsgardError asgardError;
 
-    
 	/**
 	 * 
 	 */
@@ -25,29 +23,29 @@ public class BaseException extends Exception {
 	}
 
 	/**
-	 * Creates the BrowseException, sets errorcode, erromessage.
+	 * Creates the sets errorcode, erromessage.
+	 * 
 	 * @param asgardError
 	 * @param ex
 	 */
-	public BaseException(AsgardError asgardError)
-    {
+	public BaseException(AsgardError asgardError) {
 		super(asgardError.getCustomMessage());
-		this.asgardError = asgardError;         
-    }
-	
+		this.asgardError = asgardError;
+	}
+
 	/**
-	 * Creates the BrowseException, sets errorcode, erromessage and gets the Exception .
+	 * Creates the BrowseException, sets errorcode, erromessage and gets the
+	 * Exception .
+	 * 
 	 * @param asgardError
 	 * @param ex
 	 */
-	public BaseException(AsgardError asgardError, Exception ex)
-    {
+	public BaseException(AsgardError asgardError, Exception ex) {
 		super(ex);
 		this.asgardError = asgardError;
-		 
-    }
 
-     
+	}
+
 	public BaseException(String errorCode, String errorMessage) {
 		super(errorMessage);
 		this.asgardError = new AsgardError(errorCode, errorMessage);
@@ -60,7 +58,4 @@ public class BaseException extends Exception {
 	public void setAsgardError(AsgardError asgardError) {
 		this.asgardError = asgardError;
 	}
-
-	 
-     
 }
